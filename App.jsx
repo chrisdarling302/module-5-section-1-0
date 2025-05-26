0238db2ac61381431ad854d1ee226c8c3f753a1e
Chris <cldarling@gmail.com>
2025-05-26 18:24:43 -0400

    useState complete

--- App.jsx
+++ App.jsx
@@ -2,13 +2,23 @@ import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
 import Modal from "./components/Modal.jsx";
+import React, { useState } from "react";
+import Counter from "./components/Counter.jsx";
 
 function App() {
-  let isModalOpen = false;
+  const [showModal, setShowModal] = useState(false);
 
-  function toggleModal() {
-    isModalOpen = !isModalOpen
-    console.log(isModalOpen)
+  function onTodoDelete() {
+    setShowModal(true);
+    console.log("onTodoDelete");
+  }
+
+  function cancelModal() {
+    setShowModal(false);
+  }
+
+  function confirmModal() {
+    setShowModal(false);
   }
   return (
     <div>
@@ -20,14 +30,20 @@ function App() {
             console.log(event.target.value);
           }}
         />
-        <button onClick={toggleModal}>Add Todo</button>
+        <button onClick={() => setShowModal(true)}>Add Todo</button>
       </div>
       <div className="todo_wrapper">
-        <Todo title="Finish Frontend Simplified" />
-        <Todo title="Finish Interview Section" />
-        <Todo title="Land a $100k Job" />
+        <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified" />
+        <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section" />
+        <Todo onTodoDelete={onTodoDelete} title="Land a $100k Job" />
       </div>
-      {isModalOpen && <Modal title="Confirm Delete?" />}
+      {showModal && (
+        <Modal
+          cancelModal={cancelModal}
+          confirmModal={confirmModal}
+          title="Confirm Delete?"
+        />
+      )}
     </div>
   );
 }
